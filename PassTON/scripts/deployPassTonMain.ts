a6feae36be98f4456a028b2c1b23a136be3d1c0c
Mehmet Kırçal <mehmet@notuslabs.org>
2023-02-14 17:38:57 +0300

    Test Contract

--- PassTON/scripts/deployPassTonMain.ts
+++ PassTON/scripts/deployPassTonMain.ts
@@ -5,7 +5,7 @@ import { compile, NetworkProvider } from '@ton-community/blueprint';
 export async function run(provider: NetworkProvider) {
     const passTonMain = PassTonMain.createFromConfig({}, await compile('PassTonMain'));
 
-    await provider.deploy(passTonMain, toNano('0.05'));
+    await provider.deploy(passTonMain, toNano('1'));
 
     const openedContract = provider.open(passTonMain);
 
